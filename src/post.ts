708f955a02d793e1454d23b19ff0ba76461c06c6
Kurt von Laven <kurt.von.laven@gmail.com>
2022-04-22 20:06:40 -0700

    fix(post): Inform when post step has nothing to do
    Most of the official GitHub cache action's logs wind up in our logs by
    virtue of using their API. However, we make no cache API call in the
    post step on cache hit. Manually mirror the cache action's log output in
    this case.

--- src/post.ts
+++ src/post.ts
@@ -1,16 +1,18 @@
 import { saveCache } from "@actions/cache";
-import { getInput, getState } from "@actions/core";
+import { getInput, getState, info } from "@actions/core";
 
 import { CACHE_HIT, DOCKER_IMAGES_PATH, execBashCommand } from "./util";
 
 const saveDockerImages = async (): Promise<void> => {
-  if (!getState(CACHE_HIT)) {
+  const key = getInput("key", { required: true });
+  if (getState(CACHE_HIT)) {
+    info(`Cache hit occurred on the primary key ${key}, not saving cache.`);
+  } else {
     await execBashCommand(
       'docker image list --format "{{ .Repository }}:{{ .Tag }}" | ' +
         'xargs --delimiter="\n" --no-run-if-empty --verbose --exit ' +
         `docker save --output ${DOCKER_IMAGES_PATH}`
     );
-    const key = getInput("key", { required: true });
     await saveCache([DOCKER_IMAGES_PATH], key);
   }
 };
