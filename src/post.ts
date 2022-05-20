258b833911a1e06fed83e425130919d20b3e1d34
Kurt von Laven <kurt.von.laven@gmail.com>
2022-05-20 16:39:44 -0700

    fix(post): Save cache on cache miss
    getState always returns a string, so the boolean values we pass to
    saveState are coerced to "false" and "true." Both of these are truthy
    values, so the action always incorrectly stated that a cache hit
    occurred without ever saving the cache. Test explicitly for the string
    "true" rather than for truthiness so that the cache is saved on miss.
    
    When saving Docker images on cache miss, redirect xargs --verbose output
    from stderr to stdout since it typically isn't an error.

--- src/post.ts
+++ src/post.ts
@@ -5,12 +5,12 @@ import { CACHE_HIT, DOCKER_IMAGES_PATH, execBashCommand } from "./util";
 
 const saveDockerImages = async (): Promise<void> => {
   const key = getInput("key", { required: true });
-  if (getState(CACHE_HIT)) {
+  if (getState(CACHE_HIT) === "true") {
     info(`Cache hit occurred on the primary key ${key}, not saving cache.`);
   } else {
     await execBashCommand(
       'docker image list --format "{{ .Repository }}:{{ .Tag }}" | ' +
-        'xargs --delimiter="\n" --no-run-if-empty --verbose --exit ' +
+        '2>&1 xargs --delimiter="\n" --no-run-if-empty --verbose --exit ' +
         `docker save --output ${DOCKER_IMAGES_PATH}`
     );
     await saveCache([DOCKER_IMAGES_PATH], key);
